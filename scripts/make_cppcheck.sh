6c459d7b08d73abc942921249371021452fe9e68
Pablo <pablo @aleriontec.com>
2019-10-09 07:54:41 -0700

    enable cpp check script to work in multiple repos

--- scripts/make_cppcheck.sh
+++ scripts/make_cppcheck.sh
@@ -4,7 +4,7 @@ echo '#!/bin/bash' > copy_cppcheck.sh
 for target in $(make help | grep cppcheck | grep -v '\<all_cppcheck\>' | awk '{print $2}'); do
     mkdir $target
     make $target > $target/cppcheck-result.log 2> $target/cppcheck-result.xml
-    sed -i -e 's@/opt/kpsr-core/@@g' $target/cppcheck-result.xml
-    echo "mkdir -p $target && docker cp \$1:/opt/kpsr-core/build/$target/cppcheck-result.xml $target" >> copy_cppcheck.sh
+    sed -i -e 's@/opt/$1/@@g' $target/cppcheck-result.xml
+    echo "mkdir -p $target && docker cp \$1:/opt/$1/build/$target/cppcheck-result.xml $target" >> copy_cppcheck.sh
 done
 
