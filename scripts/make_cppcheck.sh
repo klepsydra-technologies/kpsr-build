3f9ee4e87fa694a6a2a76b71d3e8b4d3ac534100
Franco Bugnano <franco@bugnano.it>
2019-11-04 10:36:45 +0100

    Bugfix in make_cppcheck.sh

--- scripts/make_cppcheck.sh
+++ scripts/make_cppcheck.sh
@@ -6,7 +6,7 @@ echo '#!/bin/bash' > copy_cppcheck.sh
 for target in $(make help | grep cppcheck | grep -v '\<all_cppcheck\>' | awk '{print $2}'); do
     mkdir $target
     make $target > $target/cppcheck-result.log 2> $target/cppcheck-result.xml
-    sed -i -e 's@/opt/$1/@@g' $target/cppcheck-result.xml
+    sed -i -e "s@/opt/$1/@@g" $target/cppcheck-result.xml
     echo "mkdir -p $target && docker cp \$1:/opt/$1/build/$target/cppcheck-result.xml $target"
     echo "mkdir -p $target && docker cp \$1:/opt/$1/build/$target/cppcheck-result.xml $target" >> copy_cppcheck.sh
 done
